b91c97853374b30a6f3ad7ad0e408a2366f858f2
SANKALP1011 <tigerishan426@gmail.com>
2022-04-04 00:11:44 +0530

    Added modules for the ui features..

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useRef , useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Lottie from "lottie-web";
 import Naruto from "../Assets/Naruto.json";
 import Tilt from "react-tilt";
+import {init} from "ityped";
 
 
 export const LandingPage = () =>{
 
-    React.useEffect(() => {
-        Lottie.loadAnimation({
-          container: document.querySelector("#Naruto"),
-          animationData: Naruto,
-        });
-      }, []);
+   const LandingText = useRef();
+
+
+      useEffect(() => {
+        init(LandingText.current, { showCursor: false, strings: [ 'アニメ','Anime'] })
+        },[]);
+    
+        React.useEffect(() => {
+          Lottie.loadAnimation({
+            container: document.querySelector("#Naruto"),
+            animationData: Naruto,
+          });
+        }, []);
+
     return<>
     <Tilt>
     <div id="Naruto" />
     </Tilt>
 
-    <h1>Welcome to the anime app.</h1>
+    <div className="LandingText">
+      <h1><span ref={LandingText}></span></h1>
+    </div>
+
     <NavLink exact to='/Anime'>
        <button>Animes</button>
     </NavLink>
