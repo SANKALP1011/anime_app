dbf6151508fa2a7e8407eb094f8209308e6dbc07
SANKALP1011 <tigerishan426@gmail.com>
2022-04-04 22:30:57 +0530

    Wroking with the frontend..

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -5,6 +5,7 @@ import Naruto from "../Assets/Naruto.json";
 import Tilt from "react-tilt";
 import {init} from "ityped";
 import Swal from "sweetalert2";
+import "../Styles/index.css";
 
 
 export const LandingPage = () =>{
@@ -32,14 +33,18 @@ export const LandingPage = () =>{
         }, []);
 
     return<>
-    <Tilt>
-    <div id="Naruto" />
-    </Tilt>
-
+    <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
     <div className="LandingText">
       <h1><span ref={LandingText}></span></h1>
-      <button type="button" onClick={DeveloperAlert} className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">hhdsh</button>
+      <button type="button" onClick={DeveloperAlert} className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Developer</button>
     </div>
+    <div id="Naruto"></div>
+    </div>
+    <Tilt>
+    
+    </Tilt>
+
+    
     <NavLink exact to='/Anime'>
        <button>Animes</button>
     </NavLink>
