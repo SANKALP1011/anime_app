c39c08e542048e7c09260604212a3bc82152d4db
SANKALP1011 <tigerishan426@gmail.com>
2022-04-04 23:16:10 +0530

    Deployement commit to test the web app on mobile..

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -46,6 +46,9 @@ export const LandingPage = () =>{
          - This is an anime app that consumes anime api inorder to fetch the data and display it to the user.
       </p>
       <p className="SubText">
+         - Do hover over the animation of naruto eating ramen to see the 3-d movement.
+      </p>
+      <p className="SubText">
         “People’s lives don’t end when they die, it ends when they lose faith.” - Uchiha Itachi
       </p>
       <button type="button" onClick={DeveloperAlert} className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
