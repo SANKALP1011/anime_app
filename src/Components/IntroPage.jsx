803ba143a18c009a85f3a08b05f7c243ecd75fd0
SANKALP1011 <tigerishan426@gmail.com>
2022-04-05 00:16:31 +0530

    Started working on the landing page..

---
+++ src/Components/IntroPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Lottie from "lottie-web";
+import IntroAnimation from "../Assets/IntroAnimation.json";
+
+export const IntroPage = () =>{
+    
+
+}
+
+export default IntroPage;
