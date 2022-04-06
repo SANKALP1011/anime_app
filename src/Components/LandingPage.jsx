d4144663d4489e3a89535251724cea7b51ba1e26
SANKALP1011 <tigerishan426@gmail.com>
2022-04-06 23:31:12 +0530

    Project done..

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -27,6 +27,7 @@ export const LandingPage = () =>{
     <div id="IntroAnimation"></div>
     <div className="IntroPageDiv">
     <h1 className="HeadingText"><span ref={IntroText}></span></h1>
+    <p className="SubText">For the best experience , visit this site on desktop.</p>
     <NavLink exact to="/Main">
     <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Click here</button>
     </NavLink>
