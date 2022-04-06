94a5bebfb7340881e48f09d904c97b3fae033e15
SANKALP1011 <tigerishan426@gmail.com>
2022-04-06 21:27:30 +0530

    Error fixed in the id route....

--- src/Components/AnimeDetail.jsx
+++ src/Components/AnimeDetail.jsx
@@ -1,37 +1,50 @@
 import React, {useState,useEffect}from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Value from "autoprefixer/lib/value";
 
 export const AnimeDetails = () =>{
 
-    const {id} = useParams({});
-    const [details,setDetails] = useState([]);
+    const {id} = useParams();
+    const [details,setDetails] = useState({});
 
     console.log(id);
-
-
-    const getAnimeDetails = () =>{
-        axios.get(`https://ghibliapi.herokuapp.com/films/${id}`)
-        .then((response)=>{
-            setDetails(response.data);
-            console.log(response.data)
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-    }
-
+  
     useEffect(()=>{
         getAnimeDetails();
-    },[id]);
+    });
 
-    return<>
-        {details.map((value)=>(
-             <div key={value.id}>
-                <h1>{value.original_title}</h1>
-             </div>
-   ))}
- </>
+    const getAnimeDetails = () =>{
+       fetch(`https://ghibliapi.herokuapp.com/films`)
+       .then(res =>{
+           if(res.ok){
+               return res.json()
+           }
+           else{
+               console.log("khkhkhkjhkhkh")
+           }
+       })
+       .then(data => {
+           const anime = data.find((animeList) => animeList.id === id);
+           console.log(anime)
+           setDetails(anime)
+       })
+       .catch(err => console.log(err))
+       
+    }
+
+   
+
+   return<>
+   <div className="DetailsCard">
+       
+   </div>
+      <h1>{details.title}</h1>
+   </>
+
+   
+      
+ 
 
 }
 
