4b5af359342a5ddb8be3dac77d9a582a9abdfb34
SANKALP1011 <tigerishan426@gmail.com>
2022-04-06 23:56:03 +0530

    Final commit.

--- src/Components/Anime.jsx
+++ src/Components/Anime.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
@@ -11,52 +11,53 @@ import "../Styles/index.css";
 import { init } from "ityped";
 import Swal from "sweetalert2";
 
-export const Anime = () =>{
+export const Anime = () => {
 
   const AnimeApiText = useRef();
 
   useEffect(() => {
-    init(AnimeApiText.current, { 
-       showCursor: true,
-       backSpeed: 60,
-       backDelay: 1500,
-       strings: [ 'Movies','映画'] })
-    },[]);
-    
-    useEffect(() =>{
-        aos.init({duration:2000});
-      },[]);
+    init(AnimeApiText.current, {
+      showCursor: true,
+      backSpeed: 60,
+      backDelay: 1500,
+      strings: ['Movies', '映画']
+    })
+  }, []);
 
-    const [anime,setAnime] = useState([]);
+  useEffect(() => {
+    aos.init({ duration: 2000 });
+  }, []);
 
-    const fetchAnime = () =>{
-        axios.get("https://ghibliapi.herokuapp.com/films")
-        .then((response)=>{
-            console.log(response)
-            setAnime(response.data);
-        })
-    }
+  const [anime, setAnime] = useState([]);
 
-    useEffect(() => {
-        fetchAnime();
-      }, []);
+  const fetchAnime = () => {
+    axios.get("https://ghibliapi.herokuapp.com/films")
+      .then((response) => {
+        console.log(response)
+        setAnime(response.data);
+      })
+  }
 
-      React.useEffect(() => {
-        Lottie.loadAnimation({
-          container: document.querySelector("#Kakashi"),
-          animationData: Kakashi,
-        });
-      }, []);
+  useEffect(() => {
+    fetchAnime();
+  }, []);
 
-    
-
-      const getQuotes = () =>{
-        Swal.fire("Naruto",
-        "If you don’t like your destiny, don’t accept it.")
-      }
+  React.useEffect(() => {
+    Lottie.loadAnimation({
+      container: document.querySelector("#Kakashi"),
+      animationData: Kakashi,
+    });
+  }, []);
 
 
-    return<>
+
+  const getQuotes = () => {
+    Swal.fire("Naruto",
+      "If you don’t like your destiny, don’t accept it.")
+  }
+
+
+  return <>
     <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
       <div className="LandingText" data-aos="fade-down-right">
         <h1 className="HeadingText">
@@ -72,34 +73,34 @@ export const Anime = () =>{
           - Do hover over each and evry anime card to see the 3-d effect along with some hover animation.
         </p>
         <button type="button" onClick={getQuotes} className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-        Quotes
-      </button>
+          Quotes
+        </button>
       </div>
       <div id="Kakashi" data-aos="fade-down-left"></div>
     </div>
 
-    
+
     <div className="item-container">
-   {anime.map((value)=>(
-       <Tilt>
- <div className="card" data-aos="fade-down-left" key={value.id}>
-           <img className="AnimeImage" data-aos="fade-down-right"  key={value.id} src={value.image}></img>
-           <h1 className="CardTextTitle" data-aos="fade-down-left"  key={value.id}>{value.title}</h1>
-           <NavLink  exact to={`/Anime/${value.id}`}>
-              <button data-aos="fade-down-right"  type="button"  className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Details</button>
-           </NavLink>
+      {anime.map((value) => (
+        <Tilt>
+          <div className="card" data-aos="fade-down-left" key={value.id}>
+            <img className="AnimeImage" data-aos="fade-down-right" key={value.id} src={value.image}></img>
+            <h1 className="CardTextTitle" data-aos="fade-down-left" key={value.id}>{value.title}</h1>
+            <NavLink exact to={`/Anime/${value.id}`}>
+              <button data-aos="fade-down-right" type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Details</button>
+            </NavLink>
 
           </div>
-       </Tilt>
-          
-       ))}
-   </div>
+        </Tilt>
+
+      ))}
+    </div>
 
 
 
 
 
-    </>
+  </>
 }
 
 export default Anime;
